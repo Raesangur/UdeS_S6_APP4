b13b602ab13bb9cd222398d7e1b86122e2f0caca
raesangur <pascalemmanuel.lachance@gmail.com>
2023-06-19 11:53:22 -0400

    serial monitor reception working

--- prob/main/main.c
+++ prob/main/main.c
@@ -6,13 +6,21 @@
 
 #include "globaldef.h"
 #include "timer.h"
+#include "serial_reception.h"
+
 
 void app_main()
 {
-    setup_timer(1);
-    
+    //setup_timer(1);
+
+    xTaskCreate(&reception_task, "Serial Reception",   2048, NULL, 5, NULL);
+
     while(true)
     {
         vTaskDelay(pdMS_TO_TICKS(100));
+        if (has_serial_reception())
+        {
+            printf("%s", get_serial_buffer());
+        }
     }
 }
