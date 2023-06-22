6ab7137402e395005218af59a1a15137398bbed8
philoup26 <teamtetras@gmail.com>
2023-06-22 16:14:12 -0400

    integrated reception

--- prob/main/main.c
+++ prob/main/main.c
@@ -17,25 +17,26 @@ void app_main()
     setup_timer(4800);
     setup_gpio();
 
-    // xTaskCreate(&reception_task, "Serial Reception", 2048, NULL, 5, NULL);
-
-    create_message("Bon matin!");
-    char* data = get_tx_data_buffer();
-    printf("Sending: %s\n", data);
-
-    size_t dataLength = strlen(data);
-
-    char* receptionData = get_rx_raw_data_buffer();
-    char* end = receptionData + dataLength;
+    xTaskCreate(&reception_task, "Serial Reception", 2048, NULL, 5, NULL);
+    
+    char* data;
+    size_t dataLength;
+    char* receptionData;
+    char* end;
 
     
     while(true)
     {
-        //vTaskDelay(pdMS_TO_TICKS(1));
-        //if(has_serial_reception())
-        //{
-        //    printf("%s", get_serial_buffer());
-        //}
+        vTaskDelay(pdMS_TO_TICKS(1));
+        if(has_serial_reception())
+        {
+            create_message(get_serial_buffer());
+            data = get_tx_data_buffer();
+            printf("Sending: %s\n", data);
+            dataLength = strlen(data);
+            receptionData = get_rx_raw_data_buffer();
+            end = receptionData + dataLength;
+        }
         if (get_and_clear_reception_byte() >= end)
         {
             printf("Sent: %s\nRecv: %s\n", data, receptionData);
